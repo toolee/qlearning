36bc09fc8397d03ef06e332e9017eb48720134fd
Too Lee <hojotoolee2@gmail.com>
2015-03-18 23:32:08 -0400

    initial

---
+++ qlearn.m
@@ -0,0 +1,19 @@
+function qlearn
+
+% 0) Create map, Draw the map
+%
+% It should show Q values, Destination.  For each episode, update the Q
+% values.
+%
+
+% 1) convert map to immediate reward matrix (node base)
+% 100  - goal
+% 1    - clear path
+% -inf - obstacle
+
+% 2) set up constants alpha, gamma, probabilities of movement, 
+% num of episodes
+
+% 3) loop thru num of episodes
+
+% 4) Traverse from any starting point

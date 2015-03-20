4ba39e284d4ef2590d857d5cc0615af9058881f6
Too Lee <hojotoolee2@gmail.com>
2015-03-20 03:31:02 -0400

    add validate map, change R population

--- qlearn.m
+++ qlearn.m
@@ -16,69 +16,94 @@ small_map = ...
       [ C, C, C, C;
         C, O, O, C;
         C, S, O, C;
-        C, O, O, G;
+        C, O, C, G;
         ];
 small_map2 = ...
-      [ S, O, C;
-        C, G, C;
+      [ S, O;
+        C, G;
         ];
 
-map = small_map2;
+map = small_map;
+
 
 global ROW;
 global COL;
 [ROW,COL] = size(map);
 
+% Validate map, and capture start, goal position
+num_start = 0;
+num_goal = 0;
+for r = 1:ROW
+  for c = 1:COL
+    if( map(r,c) == S )
+      num_start = num_start + 1;
+      start_r = r; start_c = c;
+    end
+    if( map(r,c) == G )
+      num_goal = num_goal + 1;
+      goal_r = r; goal_c = c;
+    end
+  end
+end
+if( num_start > 1 || num_goal > 1 )
+  display('Too many start or goal');
+  exit 0;
+end
+
 % 1) convert map to immediate reward matrix (node base)
 % 100  - goal
-% 1    - clear path
-% -inf - obstacle
-% 0    - not reachable, or self
+% 0    - clear path
+% -inf - obstacle, self, not reachable, diagonal...
 
 num_states = ROW*COL;
+start_state = rc2indx(start_r,start_c);
+goal_state = rc2indx(goal_r,goal_c);
 R(num_states,num_states) = 0;
 
 % 1st loop represent current node
 % 2nd loop represent visiting node, check if it is clear path, or obstacle
 for qx = 1:num_states
-    [cr,cc] = indx2rc(ROW,COL,qx);  % (c)urrent (r)ow and (c)ol
-    if( map(cr,cc) == O )
+    [cr,cc] = indx2rc(qx);  % (c)urrent (r)ow and (c)ol
+    if( map(cr,cc) == O ) % current node is an obstacle
       R(qx,:) = -inf;
       continue;
     end
     for qy = 1:num_states
-        [vr,vc] = indx2rc(ROW,COL,qy);
-        if ( abs(vr-cr) == 1 && cc == vc )
-          if ( map(vr,vc) == C )
-            R(qx,qy) = 1;
+        [vr,vc] = indx2rc(qy);
+        if ( abs(vr-cr) == 1 && cc == vc ) % +/1 row
+          if ( map(vr,vc) == C || map(vr,vc) == S )
+            R(qx,qy) = 0;
           elseif( map(vr,vc) == G )
             R(qx,qy) = 100;
-          elseif( map(vr,vc) == S )
-            R(qx,qy) = 1;
           else
             R(qx,qy) = -inf;
           end
-        elseif (abs(vc-cc) == 1 && cr == vr )
-          if ( map(vr,vc) == C )
-            R(qx,qy) = 1;
+        elseif (abs(vc-cc) == 1 && cr == vr ) % +/- col
+          if ( map(vr,vc) == C || map(vr,vc) == S )
+            R(qx,qy) = 0;
           elseif( map(vr,vc) == G )
             R(qx,qy) = 100;
-          elseif( map(vr,vc) == S )
-            R(qx,qy) = 1;
           else
             R(qx,qy) = -inf;
           end
-        else
-          R(qx,qy) = 0;
+        else % diagonal
+          R(qx,qy) = -inf;
         end
     end
 end
 
-R
-% 2) set up constants alpha, gamma, probabilities of movement, 
-% num of episodes
 
-% 3) loop thru num of episodes
+% 2) set up constants alpha, gamma, probabilities of movement, 
+global ALPHA; ALPHA = 0.1;
+global GAMMA; GAMMA = 0.2;
+global EPISODES; EPISODES = 100;
+
+% 3) loop thru num of EPISODES
+for e = 1:EPISODES
+  init_state = randperm(num_states,1);
+  while init_state ~= goal_state
+  end
+end
 
 % 4) Traverse from any starting point
 
@@ -87,10 +112,14 @@ R
 %--------------------------------------------------------------------------
 % util
 %--------------------------------------------------------------------------
-function index = rc2indx(ROW,COL,r,c)
+function index = rc2indx(r,c)
+global ROW;
+global COL;
 index = (r-1)*COL+c;
 
-function [r,c] = indx2rc(ROW,COL,i)
+function [r,c] = indx2rc(i)
+global ROW;
+global COL;
 r = ceil(i/COL);
 c = mod(i,COL);
 if( c == 0 )

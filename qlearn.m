b33647d6b192b1e08428d7fc9dcca1eeaf4304f9
Too Lee <hojotoolee2@gmail.com>
2015-03-19 21:19:27 -0400

    add small_map

--- qlearn.m
+++ qlearn.m
@@ -2,14 +2,25 @@ function qlearn
 
 % 0) Create map, Draw the map
 %
-% It should show Q values, Destination.  For each episode, update the Q
-% values.
+% It should show Q values, Destination.  
+% For each episode, update the Q values.
 %
+
+small_map = ...
+      [ C, C, C, C;
+        C, O, O, C;
+        C, S, O, C;
+        C, O, O, G;
+        ];
+    
+map = small_map;
 
 % 1) convert map to immediate reward matrix (node base)
 % 100  - goal
 % 1    - clear path
 % -inf - obstacle
+
+
 
 % 2) set up constants alpha, gamma, probabilities of movement, 
 % num of episodes
